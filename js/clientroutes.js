250928656862a009f4daed1dceb9ef77fcd83501
Tommy Messbauer <tommy@vast.com>
2013-09-30 12:14:45 -0500

    updated to remove all backbone reminants

--- js/clientroutes.js
+++
@@ -1,6 +0,0 @@
-// Names of the functions attached to the router.
-module.exports = {
-  home: 'home', 
-  geo: 'geo', 
-  map: 'map'
-};
--- js/events/eventEmitter.js
+++ js/events/eventEmitter.js
@@ -1,16 +1,16 @@
 // attach pubsub to global namespace
-
 var events = require('events');
 var em = global.eventEmitter;
-var _ = require('lodash');
 
 global.eventEmitter = new events.EventEmitter();
 
 // add all of the events from the shim.  The shim is inline script at the top of the page.
 if (em) {
-  _.each(em._events, function(evs, name) {
-    _.each(evs, function(fn) {
+  var events = em._events || {};
+  for(name in events) {
+
+    events[name].forEach(function(fn) {
       eventEmitter.on(name, fn);
     });
-  });
+  }
 }
